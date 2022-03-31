c32707ab2de93eae73fd4df0021f30d5eecf49c6
Mohammad Shahanwaz <mohdshahanwaz55@gmail.com>
2022-03-31 22:55:39 +0530

    Made design

--- components/Profile/Profile.tsx
+++ components/Profile/Profile.tsx
@@ -3,5 +3,9 @@ import styles from "./Profile.module.css";
 export default function Profile(props: any) {
   const { data } = props;
 
-  return <div className={styles.profile}></div>;
+  return (
+    <div className={styles.profile}>
+      <p>{data.name}</p>
+    </div>
+  );
 }
--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -2,10 +2,6 @@ import * as React from "react";
 import { useRouter } from "next/router";
 import Profile from "../../components/Profile";
 
-interface ID {
-  id: string;
-}
-
 export default function User() {
   const router = useRouter();
   const { id } = router.query;
